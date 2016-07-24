c48d361e63b3b37ca823d414bd53059fc1db802c
Stark-Winteriscomming <chrisvan@naver.com>
2016-07-24 17:12:18 +0900

    Add files via upload

---
+++ StudentController.java
@@ -0,0 +1,39 @@
+package Controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+import dao.impl.studentDaoimpl;
+
+public class StudentController extends AbstractController{
+	
+	private studentDaoimpl studentDaoimpl;
+	public void setStudentDaoimpl(studentDaoimpl studentDaoimpl)
+	{
+		this.studentDaoimpl = studentDaoimpl;
+	}
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest arg0,
+			HttpServletResponse arg1) throws Exception {
+		// TODO Auto-generated method stub
+		List studentinfo = this.studentDaoimpl.getStudentinfo();		
+		Map model = new HashMap();
+		model.put("studentinfo",studentinfo);
+		ModelAndView mav = new ModelAndView();
+		
+		mav.setViewName("/student");
+				
+		mav.addAllObjects(model);		
+	
+		return mav;
+	}
+
+}
